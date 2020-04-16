af8f6eaf23069f7ee705d19541617549a86ec7aa
Sandra Daubigne <44670927+SandraDaubigne@users.noreply.github.com>
2020-04-16 13:40:40 +0200

    POJO class ready

--- src/main/java/project_backend/Todo.java
+++ src/main/java/project_backend/Todo.java
@@ -1,4 +1,34 @@
 package project_backend;
 
+//My POJO class that will serve as an object in this code
 public class Todo {
+
+    private int id;
+    private String text;
+    private boolean active;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public boolean getActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
 }
+
